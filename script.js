fec6ca478c10062602ff04b3bb8eb1aea62d0506
TKuhner <trevorkuhner29@gmail.com>
2023-05-18 09:09:19 -0500

    program passed all of my tests. added comments to better show what is occuring

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ var upperAlph = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 
 var lowerAlph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-var numChar = ["0","1","2", "3", "4", "5", "6", "7", "8", "9"]
+var numChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-var specChar = ["!", "#", "$", "%", "^", "&", "*", "(", ")", ".", ",", "?"] 
+var specChar = ["!", "#", "$", "%", "^", "&", "*", "(", ")", ".", ",", "?"]
 
 
 // Assignment Code
@@ -16,116 +16,124 @@ function writePassword() {
     // length of password to be passed as for loop controller
     var userLength = getLength();
 
-    // booleans to
+    // userChoices array to control switch casing
+    var userChoices = []
+
+    // booleans to create case options in switch
     var userUpper = confirm("Select 'OK' to include upper case letters.");
     var userLower = confirm("Select 'OK' to include upper case letters.");
     var userNumber = confirm("Select 'OK' to include numbers.");
     var userSpecial = confirm("Select 'OK' to include special case letters.");
 
-    var userChoices = []
 
-    if(userUpper === true){
+    // adds choice options to choice array
+    if (userUpper === true) {
         userChoices.push(0)
     }
-    if(userLower === true){
+    if (userLower === true) {
         userChoices.push(1)
     }
-    if(userNumber === true){
+    if (userNumber === true) {
         userChoices.push(2)
     }
-    if(userSpecial === true){
+    if (userSpecial === true) {
         userChoices.push(3)
     }
 
-    console.log("userChoices " + userChoices);
+    // tester to see what choices made it into the choice array
+    // console.log("userChoices " + userChoices);
 
 
+    // password variable == generatePassword with passed parameters 
+    var password = generatePassword(userLength, userChoices);
+    var passwordText = document.querySelector("#password");
 
-  var password = generatePassword(userLength, userChoices);
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword(numLength, choiceArr){
-
+function generatePassword(numLength, choiceArr) {
+    // create new password as a string
     var newPassword = "";
 
-    
+    // loop for length of user's chosen length
     for (let index = 0; index < numLength; index++) {
-        
-        var x = Math.floor(Math.random() * choiceArr.length);
-        console.log("X" + x)
 
+        // random number for length of choice array
+        var x = Math.floor(Math.random() * choiceArr.length);
+
+
+        // x has a position value and needs to be reassigned to the actual choice at that position
         x = choiceArr[x]
 
-        console.log("X" + x)
-
+        // declaring y value so it is re-initialized after every iteration of the for loop
         var y;
 
 
-        switch(x){
+        switch (x) {
             case 0:
+                // confirmation that cases are being accessed as needed and ignorder as needed
                 console.log("made it to case 0")
-                
+
+                // y has a positional value that is used to pull the character at that position from the array that is paired to this case
                 y = Math.floor(Math.random() * upperAlph.length);
-                
-                console.log("y = " + y);
+
+
 
                 newPassword += upperAlph[y]
-                
+
                 break;
             case 1:
+                // confirmation that cases are being accessed as needed and ignorder as needed
                 console.log("made it to case 1")
-                
+
+                // y has a positional value that is used to pull the character at that position from the array that is paired to this case
                 y = Math.floor(Math.random() * lowerAlph.length);
-                
-                console.log("y = " + y);
 
                 newPassword += lowerAlph[y];
-                
+
                 break;
             case 2:
+                // confirmation that cases are being accessed as needed and ignorder as needed
                 console.log("made it to case 2")
-                
+
+                // y has a positional value that is used to pull the character at that position from the array that is paired to this case
                 y = Math.floor(Math.random() * numChar.length);
-                
-                console.log("y = " + y);
 
                 newPassword += numChar[y]
-                
+
                 break;
             case 3:
+                // confirmation that cases are being accessed as needed and ignorder as needed
                 console.log("made it to case 3")
-                
+
+                // y has a positional value that is used to pull the character at that position from the array that is paired to this case
                 y = Math.floor(Math.random() * specChar.length);
-                
-                console.log("y = " + y);
+
 
                 newPassword += specChar[y]
                 break;
         }
-        
+
     }
-    console.log(newPassword);
-    
+
     return newPassword;
 }
 
 
-function getLength(){
+function getLength() {
     // prompt user and store input
     var numLength = prompt("Please enter a length for your password that is at least 8 characters and at most 128 characters.")
 
     // error check for valid input
-    if(numLength >= 8 && numLength <= 128){
-        // return length
+    if (numLength >= 8 && numLength <= 128) {
+        // return length if parameter is acceptable
         return numLength;
-    }else{
+    } else {
+        // if paramater is not acceptable, alert user and re-prompt
         alert("You've entered an invalid value. Please enter a valid input.")
         getLength();
     }
